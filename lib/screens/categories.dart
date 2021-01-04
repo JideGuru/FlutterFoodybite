2dd49396d27eba75476aa5dbe624c1b99525bee2
Shamsudeen Adedokun <adedokunshamsudeen@gmail.com>
2021-01-04 19:18:38 +0100

    Created build restaurant row to allow different see all links

---
+++ lib/screens/categories.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
